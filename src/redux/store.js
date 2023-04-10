0b2dc215c52b87380b2031d2ee970026e6126f37
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-04-10 23:32:00 +0300

    initial redux

---
+++ src/redux/store.js
@@ -0,0 +1,39 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from './auth/sliceAuth';
+import { contactsReducer } from './contacts/sliceContacts';
+
+// Persisting token field from auth slice to localstorage
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
+  },
+  middleware,
+});
+
+export const persistor = persistStore(store);
