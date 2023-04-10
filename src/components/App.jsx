096c617f2d92fdbb1112fb3e0fa9ee4c17dc7e98
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-04-10 22:53:52 +0300

    added routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
+import React, { lazy } from 'react';
+import { Toaster } from 'react-hot-toast';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+//layoutes and pages lazy loading
+import MainLayout from 'layouts/MainLayout';
+const StartPage = lazy(() => import('pages/StartPage'));
+const RegisterPage = lazy(() => import('pages/RegisterPage'));
+const LoginPage = lazy(() => import('pages/LoginPage'));
+const CalendarPage = lazy(() => import('pages/CalendarPage'));
+const AccountPage = lazy(() => import('pages/AccountPage'));
+const NoMatchPage = lazy(() => import('pages/StartPage'));
+
+//basename for react-router
+const basename = '';
+
+//router and routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<StartPage />} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="register" element={<RegisterPage />} />
+
+      <Route index element={<MainLayout />}>
+        <Route path="account" element={<AccountPage />} />
+        <Route path="calendar" element={<CalendarPage />} />
+      </Route>
+
+      <Route path="*" element={<NoMatchPage />} />
+    </Route>
+  ),
+  { basename }
+);
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <RouterProvider router={router} />
+      <div>
+        <Toaster />
+      </div>
+    </>
   );
 };
+
+export default App;
