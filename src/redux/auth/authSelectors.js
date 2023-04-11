31751f59299d13081f589e75afccad4acae5db1f
ArtemKrylov <krylovartem1994@gmail.com>
2023-04-11 21:30:20 +0300

    Delete src/redux directory

--- src/redux/auth/authSelectors.js
+++
@@ -1,5 +0,0 @@
-export const selectIsLoggedIn = state => state.auth.isLogged;
-export const selectIsUserLoading = state => state.auth.isLoading;
-export const selectUserError = state => state.auth.error;
-export const selectIsUserError = state => Boolean(state.auth.error);
-export const selectUser = state => state.auth.user;
