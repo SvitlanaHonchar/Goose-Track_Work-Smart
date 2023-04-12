969c9637b503f0fca5682063be33fdb273ece31c
SvitlanaHonchar <snidlty@gmail.com>
2023-04-12 20:27:44 +0300

    Update MainLayout.styled.js

--- src/modules/MainLayout/components/MainLayout/MainLayout.styled.js
+++ src/modules/MainLayout/components/MainLayout/MainLayout.styled.js
@@ -1,15 +1,14 @@
 import styled from 'styled-components';
 
 export const StyledDiv = styled.div`
+  display: flex;
+
   margin-left: auto;
   margin-right: auto;
-  padding-right: 32px;
-  /* padding-left: 20px; */
-
   margin-top: 20px;
   margin-bottom: 20px;
 
-  /* background-color: #eaeaea; */
+  padding-right: 32px;
 
   @media screen and (min-width: 375px) {
     width: 375px;
@@ -18,7 +17,6 @@ export const StyledDiv = styled.div`
   @media screen and (min-width: 768px) {
     width: 768px;
     padding-right: 32px;
-    /* padding-left: 32px; */
   }
 
   @media screen and (min-width: 1440px) {
