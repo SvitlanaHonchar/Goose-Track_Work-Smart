bd3e13d5f1ddbe460c42fed702ff0389bebf09f2
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-04-14 15:55:31 +0300

    add adaptive styles

--- src/modules/MainLayout/components/SideBar/Sidebar.styled.js
+++ src/modules/MainLayout/components/SideBar/Sidebar.styled.js
@@ -6,14 +6,20 @@ export const SideBarContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  flex-grow: 7, 1;
+  /* flex-grow: 7, 1; */
 
   border-right: 1px solid #dce3e5;
   height: 900px;
+
+  padding: 24px 32px;
+
+  @media screen and (max-width: 375px) {
+    padding: 24px 20px;
+  }
 `;
 
 export const StyledNavContainer = styled.nav`
-  padding: 24px;
+  /* padding: 24px; */
   background-color: #fff;
   flex-grow: 6;
 
@@ -25,7 +31,16 @@ export const StyledNavContainer = styled.nav`
 export const StyledLogoDiv = styled.div`
   display: flex;
   align-items: center;
-  padding: 8px;
+  /* padding: 8px; */
+  margin-bottom: 32px;
+
+  @media screen and (max-width: 1439px) {
+    margin-bottom: 50px;
+  }
+
+  @media screen and (max-width: 767px) {
+    margin-bottom: 64px;
+  }
 
   & h2 {
     margin-top: 0;
@@ -36,19 +51,40 @@ export const StyledLogoDiv = styled.div`
     font-size: 24px;
     line-height: 1;
     color: #3e85f3;
+
+    @media screen and (max-width: 1439px) {
+      font-size: 18px;
+      line-height: 1.33;
+    }
+
+    @media screen and (max-width: 767px) {
+      font-size: 16px;
+    }
   }
 
   & div {
     margin-right: 10px;
+  }
+
+  & img {
+    width: 71px;
+    height: 68px;
+
+    @media screen and (max-width: 1439px) {
+      width: 60px;
+    }
+
+    @media screen and (max-width: 767px) {
+      width: 36px;
+      height: 35px;
+    }
   }
 `;
 
 // export const NavigationDiv = styled.div``;
 
 export const StyledNavLink = styled(NavLink)`
-  padding-top: 18px;
-  padding-bottom: 18px;
-  padding-left: 23px;
+  padding: 18px 23px;
   text-decoration: none;
   color: #84828a;
   border-radius: 8px;
@@ -60,7 +96,13 @@ export const StyledNavLink = styled(NavLink)`
   font-style: normal;
   font-weight: 600;
   font-size: 16px;
-  line-height: 19px;
+  line-height: 1.18;
+
+  @media screen and (max-width: 767px) {
+    font-size: 14px;
+    line-height: 1.21;
+    padding: 12px 16px;
+  }
 
   & p {
     margin-bottom: 0;
@@ -91,18 +133,30 @@ export const StyledIcon = styled.div`
 
 export const StyledNavDiv = styled.div`
   & p {
-    font-family: 'Inter', sans-serif;
+    /* font-family: 'Inter', sans-serif;
     font-style: normal;
     font-weight: 600;
     font-size: 14px;
     line-height: 17px;
 
-    color: #7e7d82;
+    color: #7e7d82; */
   }
 `;
 
 export const StyledNavName = styled.p`
   margin-bottom: 32px;
+
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 1.21;
+
+  color: #7e7d82;
+
+  @media screen and (max-width: 767px) {
+    font-size: 12px;
+  }
 `;
 
 export const StyledLogoutBtn = styled.button`
@@ -113,6 +167,10 @@ export const StyledLogoutBtn = styled.button`
   max-width: 141px;
   border: none;
   padding: 16px 23px;
+
+  @media screen and (max-width: 767px) {
+    padding: 14px 28px;
+  }
 
   cursor: pointer;
 
@@ -133,6 +191,11 @@ export const StyledLogoutBtn = styled.button`
 
     letter-spacing: -0.02em;
     color: #fff;
+
+    @media screen and (max-width: 767px) {
+      font-size: 14px;
+      line-height: 1.29;
+    }
   }
 
   & svg {
--- src/pages/NoMatchPage/NoMatchPage.jsx
+++ src/pages/NoMatchPage/NoMatchPage.jsx
@@ -1,13 +1,13 @@
-// import SideBar from 'modules/MainLayout/components/SideBar/SideBar';
+import SideBar from 'modules/MainLayout/components/SideBar/SideBar';
 import React from 'react';
 
 const NoMatchPage = () => {
-  return <div>No match page</div>;
-  // return (
-  //   <>
-  //     <SideBar />
-  //   </>
-  // );
+  // return <div>No match page</div>;
+  return (
+    <>
+      <SideBar />
+    </>
+  );
 };
 
 export default NoMatchPage;
