7ca412b1e579f878cd369b4fdcfb425017bf4d7d
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2023-04-14 06:49:30 +0300

    edited LoginPageComponent

--- src/modules/LoginPageComponent/components/LoginPageComp/LoginPageComponent.jsx
+++ src/modules/LoginPageComponent/components/LoginPageComp/LoginPageComponent.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { LoginForm } from '../LoginForm/LoginForm';
+import { StyledLoginPageComponent } from './LoginPageComponent.styled';
+import theme from 'shared/theme';
 
 const LoginPageComponent = () => {
-  return <div></div>;
+  return (
+    <StyledLoginPageComponent
+      sx={{ backgroundColor: theme.palette.primary.dark }}
+    >
+      <LoginForm />
+    </StyledLoginPageComponent>
+  );
 };
 
 export default LoginPageComponent;
