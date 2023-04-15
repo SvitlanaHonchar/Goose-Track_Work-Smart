de6de31f63cfd1764f7b2cc763ee653bebd71ec5
SvitlanaHonchar <snidlty@gmail.com>
2023-04-15 22:29:29 +0300

    first commit

--- src/modules/ChosenMonth/components/CalendarTable/CalendarTable.jsx
+++ src/modules/ChosenMonth/components/CalendarTable/CalendarTable.jsx
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import CalendarDates from 'calendar-dates';
+
+const calendarDates = new CalendarDates();
 
 function CalendarTable({ tasks, currentMonth }) {
-  return <div>CalendarTable</div>;
+  const [monthMatrix, setMonthMatrix] = useState(null);
+
+  useEffect(() => {
+    const mainAsync = async () => {
+      const calendarMatrix = await calendarDates.getMatrix(new Date());
+      // console.log(`currMonth`, calendarMatrix);
+      setMonthMatrix(calendarMatrix);
+    };
+
+    mainAsync();
+  }, []);
+
+  if (monthMatrix !== null) {
+    // month
+    console.log(monthMatrix);
+    // week
+    console.log(monthMatrix[0]);
+    // day data
+    console.log(monthMatrix[0][0]);
+    // day
+    console.log(monthMatrix[0][0].date);
+  }
+
+  return (
+    <div>
+      CalendarTable
+      <table>
+        <tbody>
+          {monthMatrix !== null &&
+            monthMatrix.map((week, i) => (
+              <tr>
+                {week.map((day, j) => (
+                  <td>{day.date}</td>
+                ))}
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 CalendarTable.propTypes = {
