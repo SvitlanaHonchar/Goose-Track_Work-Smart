5bd768d392a96f464c6feafbe9cd725307f6feb7
SvitlanaHonchar <snidlty@gmail.com>
2023-04-16 23:03:25 +0300

    table add

--- src/modules/ChosenMonth/components/CalendarTable/CalendarTable.jsx
+++ src/modules/ChosenMonth/components/CalendarTable/CalendarTable.jsx
@@ -1,6 +1,6 @@
-// 1. Компонент отримує колекцію днів обраного місяця з задачами, вказаними для виконання на відповідні дні.
+// + 1. Компонент отримує колекцію днів обраного місяця з задачами, вказаними для виконання на відповідні дні.
 // 2. Якщо день має заплановані задачі, вони відображаються відповідними блоками всередині комірки дня.
-// 3. Клік по комірці переадресовує юзера на відповідний день по маршруту /calendar/day/:date і показує модуль одного дня ChoosedDay з відповідною датою.
+// + 3. Клік по комірці переадресовує юзера на відповідний день по маршруту /calendar/day/:date і показує модуль одного дня ChoosedDay з відповідною датою.
 // Додатково:
 // Клік по завданню з комірки, відкриває модалку для редагування даного завдання, заповнену даними з цього завдання.
 
@@ -8,32 +8,63 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import CalendarDates from 'calendar-dates';
 import {
-  Paper,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableRow,
 } from '@mui/material';
+import { NavLink } from 'react-router-dom';
 
 const calendarDates = new CalendarDates();
 
 function CalendarTable({ tasks, currentMonth }) {
   const [monthMatrix, setMonthMatrix] = useState(null);
 
+  const getAllDayTasks = date => {
+    if (tasks === null) {
+      return '';
+    }
+    const todayTasks = tasks.filter(task => task.date === date);
+    const todayTasksMarkup =
+      todayTasks.length !== 0
+        ? todayTasks[0].tasks.map(task => (
+            <li key={task._id} className={task.priority}>
+              {task.title}
+            </li>
+          ))
+        : '';
+
+    return todayTasksMarkup;
+  };
+
+  console.log(tasks);
+
+  function getNextDay(currDate) {
+    const nextDate = new Date(currDate);
+    nextDate.setDate(nextDate.getDate() + 1);
+
+    return nextDate.toISOString().slice(0, 10);
+  }
+
+  const yesterday = getNextDay('2023-03-26');
+  console.log(yesterday);
+
   useEffect(() => {
     const mainAsync = async () => {
-      // !!! new Date - замінити на дані з пропсів
-      const calendarMatrix = await calendarDates.getMatrix(new Date());
+      const calendarMatrix = await calendarDates.getMatrix(
+        new Date(currentMonth)
+      );
+
       setMonthMatrix(calendarMatrix);
     };
 
     mainAsync();
-  }, []);
+  }, [currentMonth]);
 
   // if (monthMatrix !== null) {
   //   // month
-  //   console.log(monthMatrix);
+  //   console.log('month', monthMatrix);
   //   // week
   //   console.log(monthMatrix[0]);
   //   // day data
@@ -45,17 +76,34 @@ function CalendarTable({ tasks, currentMonth }) {
   return (
     <div>
       CalendarTable
-      <TableContainer component={Paper}>
+      <TableContainer
+        sx={{
+          border: '1px rgba(220, 227, 229, 0.8) solid',
+          borderRadius: '8px',
+        }}
+        // component={Paper}
+      >
         <Table aria-label="simple table">
           <TableBody>
             {monthMatrix !== null &&
               monthMatrix.map((week, i) => (
-                <TableRow
-                  key={i}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
+                <TableRow key={i} align="center">
                   {week.map((day, j) => (
-                    <TableCell key={j}>{day.date}</TableCell>
+                    <TableCell
+                      key={j}
+                      align="right"
+                      sx={{
+                        verticalAlign: 'top',
+                        border: '1px rgba(220, 227, 229, 0.8) solid',
+                        height: 125,
+                        width: 155,
+                      }}
+                    >
+                      <NavLink to={`/calendar/day/${getNextDay(day.iso)}`}>
+                        {getNextDay(day.iso)}
+                      </NavLink>
+                      <ul>{getAllDayTasks(`${getNextDay(day.iso)}`)}</ul>
+                    </TableCell>
                   ))}
                 </TableRow>
               ))}
