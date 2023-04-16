9d5ecfc26d1679e1c5441664a0dac4ef5bc7f848
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2023-04-16 11:18:29 +0300

    Updated LoginForm

--- src/modules/LoginPageComponent/components/LoginForm/LoginForm.jsx
+++ src/modules/LoginPageComponent/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { Formik, Field, ErrorMessage } from 'formik';
 import { object, string } from 'yup';
@@ -8,10 +9,16 @@ import {
   StyledButton,
   StyledNavLink,
   StyledContainer,
+  StyledVisibilityBtn,
 } from './LoginForm.styled.js';
 import Icons from '../../../../shared/icons/sprite.svg';
 import { authLogin } from 'redux/auth/authOperations.js';
-import theme from '../../../../shared/theme.js';
+import {
+  showSuccessLogIn,
+  showError,
+} from '../../../../shared/utils/notifications';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import theme from 'shared/theme';
 
 const validationSchema = object({
   email: string().required('Required').email('Invalid email'),
@@ -28,29 +35,51 @@ const Icon = ({ name, width, height }) => (
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [passwordType, setPasswordType] = useState('password');
+
+  const togglePassword = () => {
+    if (passwordType === 'password') {
+      setPasswordType('text');
+      return;
+    }
+    setPasswordType('password');
+  };
+
   return (
-    <>
+    <StyledContainer>
       <Formik
         initialValues={{
           email: '',
           password: '',
         }}
         validationSchema={validationSchema}
-        onSubmit={(values, { setSubmitting, resetForm }) => {
+        onSubmit={async (values, { setSubmitting, resetForm }) => {
           console.log(values);
-          dispatch(
-            authLogin({
-              email: values.email,
-              password: values.password,
-            })
-          );
-
-          setSubmitting(false);
-          resetForm();
+          try {
+            const actionResult = await dispatch(
+              authLogin({
+                email: values.email,
+                password: values.password,
+              })
+            );
+            console.log(actionResult);
+            if (actionResult.type === 'user/login/fulfilled') {
+              showSuccessLogIn();
+              navigate('/calendar/month');
+              setSubmitting(false);
+              resetForm();
+            } else {
+              showError();
+            }
+          } catch (e) {
+            console.log(e);
+          }
         }}
       >
         {({ values, isSubmitting }) => (
-          <StyledContainer>
+          <div className="FormContainer">
             <StyledLoginForm>
               <p className="Title"> Log in</p>
               <label className="Inputlabel">
@@ -68,15 +97,26 @@ export const LoginForm = () => {
                   className="ErrorMessage"
                 />
               </label>
-              <label className="Inputlabel">
-                Password
-                <Field
-                  className="InputField"
-                  type="password"
-                  name="password"
-                  value={values.password}
-                  placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-                />
+              <label className="Inputlabel ">
+                <span>Password</span>
+                <div className="PasswordInput ">
+                  <Field
+                    className="InputField InputFieldPassword"
+                    name="password"
+                    value={values.password}
+                    placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+                    type={passwordType}
+                  />
+                  <StyledVisibilityBtn type="button" onClick={togglePassword}>
+                    {passwordType === 'password' ? (
+                      <VisibilityOff
+                        sx={{ color: theme.palette.primary.main }}
+                      />
+                    ) : (
+                      <Visibility sx={{ color: theme.palette.primary.main }} />
+                    )}
+                  </StyledVisibilityBtn>
+                </div>
                 <ErrorMessage
                   className="ErrorMessage"
                   name="password"
@@ -93,10 +133,10 @@ export const LoginForm = () => {
                 <Icon className="Login" name="logIn" width="18" height="18" />
               </StyledButton>
             </StyledLoginForm>
-          </StyledContainer>
+          </div>
         )}
       </Formik>
       <StyledNavLink to="/register">Sign in</StyledNavLink>
-    </>
+    </StyledContainer>
   );
 };
