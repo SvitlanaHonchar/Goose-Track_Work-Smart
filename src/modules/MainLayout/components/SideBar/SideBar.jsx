50e5dafdff7fef3a2296a5419e3b9f5def55ad01
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-04-17 12:56:59 +0300

    delete unnesessaty lines

--- src/modules/MainLayout/components/SideBar/SideBar.jsx
+++ src/modules/MainLayout/components/SideBar/SideBar.jsx
@@ -1,12 +1,9 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import UserNav from './UserNav';
 import LogoutBtn from './LogoutBtn';
 import { SideBarContainer } from './Sidebar.styled';
 
 function SideBar({ menu, onClose }) {
-  const screenWidth = useRef(window.innerWidth);
-  console.log('screen width', screenWidth <= 768);
-
   return (
     <SideBarContainer className={menu}>
       <UserNav onClick={onClose} />
