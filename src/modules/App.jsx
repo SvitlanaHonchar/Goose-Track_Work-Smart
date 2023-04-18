484b6584240ab6c0c37fa5c9396164b717de71ad
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-04-18 23:42:13 +0300

    solve double request

--- src/modules/App.jsx
+++ src/modules/App.jsx
@@ -6,7 +6,7 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from 'react-router-dom';
-
+import { selectUserError } from 'redux/auth/authSelectors';
 //layoutes and pages lazy loading
 import MainLayout from './MainLayout/components/MainLayout/MainLayout';
 import { authRefresh } from 'redux/auth/authOperations';
@@ -69,11 +69,13 @@ const router = createBrowserRouter(
 const App = () => {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
-
+  const error = useSelector(selectUserError);
   // Refresh - success
   useEffect(() => {
     if (user.name !== null) return;
-
+    // if (error?.status === 401) {
+    //   console.log('401 error');
+    // }
     dispatch(authRefresh());
   }, [dispatch, user.name]);
 
