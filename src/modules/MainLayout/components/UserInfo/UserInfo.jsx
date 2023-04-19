41b786822d61d3429913bc2a3afde1b60e084ffd
Natalia Karpenko <106306905+natalia-no-war@users.noreply.github.com>
2023-04-19 11:05:20 +0300

    updated UserInfo

--- src/modules/MainLayout/components/UserInfo/UserInfo.jsx
+++ src/modules/MainLayout/components/UserInfo/UserInfo.jsx
@@ -1,11 +1,29 @@
-import { Avatar, Box, Typography } from '@mui/material';
-import React from 'react';
+import { Avatar, Box, Button, Typography } from '@mui/material';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectIsUserLoading, selectUser } from 'redux/auth/authSelectors';
 import Loader from '../../../../shared/components/Loader/Loader';
 import theme from 'shared/theme';
+import UserInfoModal from './UserInfoModal';
 
-function stringToColor(string) {
+const style = {
+  button: {
+    minWidth: '32px',
+    width: {
+      xs: '32px',
+      md: '44px',
+    },
+
+    height: {
+      xs: '32px',
+      md: '44px',
+    },
+    padding: 0,
+    bgcolor: 'transparent',
+  },
+};
+
+export const stringToColor = string => {
   let hash = 0;
   let i;
 
@@ -21,70 +39,86 @@ function stringToColor(string) {
   }
 
   return color;
-}
+};
 
 const UserInfo = () => {
   const { name, userImgUrl } = useSelector(selectUser);
   const isUserLoading = useSelector(selectIsUserLoading);
 
+  const [openModal, setOpenModal] = useState(false);
+  const onOpenModal = () => setOpenModal(true);
+  const onCloseModal = () => setOpenModal(false);
+
   return isUserLoading ? (
     // <b>Loading...</b>
     <Loader />
   ) : (
-    <Box sx={{ display: 'flex', alignItems: 'center', gap: '14px' }}>
-      <Typography
-        sx={{
-          fontWeight: 700,
-          color: theme.palette.grey[500],
-          lineHeight: {
-            xs: 1.29,
-            md: 1,
-          },
-
-          fontSize: {
-            xs: 14,
-            md: 18,
-          },
-        }}
-      >
-        {name?.split(' ')[0] || ''}
-      </Typography>
-      {userImgUrl ? (
-        <Avatar
-          src={userImgUrl}
-          alt={`${name} picture`}
-          sx={{
-            width: {
-              xs: 32,
-              md: 44,
-            },
-            height: {
-              xs: 32,
-              md: 44,
-            },
-            border: `2px solid ${theme.palette.primary.main}`,
-          }}
+    <>
+      {openModal && (
+        <UserInfoModal
+          name={name}
+          userImgUrl={userImgUrl}
+          onCloseModal={onCloseModal}
         />
-      ) : (
-        <Avatar
-          alt={`${name} picture`}
+      )}
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: '14px' }}>
+        <Typography
           sx={{
-            width: {
-              xs: 32,
-              md: 44,
-              background: stringToColor(name),
+            fontWeight: 700,
+            color: theme.palette.grey[500],
+            lineHeight: {
+              xs: 1.29,
+              md: 1,
             },
-            height: {
-              xs: 32,
-              md: 44,
+
+            fontSize: {
+              xs: 14,
+              md: 18,
             },
-            // border: `2px solid ${theme.palette.primary.main}`,
           }}
         >
-          {name?.charAt(0) || ''}
-        </Avatar>
-      )}
-    </Box>
+          {name?.split(' ')[0] || ''}
+        </Typography>
+        <Button sx={style.button} onClick={() => onOpenModal()}>
+          {userImgUrl ? (
+            <Avatar
+              src={userImgUrl}
+              alt={`${name} picture`}
+              sx={{
+                width: {
+                  xs: 32,
+                  md: 44,
+                },
+                height: {
+                  xs: 32,
+                  md: 44,
+                },
+
+                border: `2px solid ${theme.palette.primary.main}`,
+              }}
+            />
+          ) : (
+            <Avatar
+              alt={`${name} picture`}
+              sx={{
+                width: {
+                  xs: 32,
+                  md: 44,
+                  background: stringToColor(name),
+                },
+                height: {
+                  xs: 32,
+                  md: 44,
+                },
+                // border: `2px solid ${theme.palette.primary.main}`,
+              }}
+            >
+              {name?.charAt(0) || ''}
+            </Avatar>
+          )}
+        </Button>
+      </Box>
+    </>
   );
 };
 
