daebae3390baf6b4ed6db6ceec4a9cd490454983
Artem <43321337+ArtemKrylov@users.noreply.github.com>
2023-04-21 13:08:56 +0300

    corrected darkMode - no bug when localStorage is empty

--- src/modules/App.jsx
+++ src/modules/App.jsx
@@ -31,10 +31,10 @@ const CalendarPageWithRedirect = withAuthRedirect(CalendarPage, '/');
 
 // ----------------------------------------------------
 // for gitHub
-const basename = '/Goose-Track_Work-Smart/';
+// const basename = '/Goose-Track_Work-Smart/';
 
 // for work
-// const basename = '';
+const basename = '';
 // ----------------------------------------------------
 
 const router = createBrowserRouter(
